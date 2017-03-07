931d819eeab45dfcb24c2e7d4b2a0c1607724aeb
dsztanko <szt.dori@gmail.com>
2017-03-07 12:16:50 +0100

    little fix

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -9,17 +9,16 @@ class Service(object):
         else:
             print(node.value)
             if node.value == target_value:
-                return "Node with value {0} is found: {1}. Target value was: {2}."
-                .format(node.value, node, target_value)
+                return "Node with value {0} is found: {1}. Target value was: {2}.".format(node.value, node, target_value)
             else:
-                node.setToVisited()
+                node.set_to_visited()
                 if node.left is None or node.left.visited is True:
                     if node.right is None or node.right.visited is True:
                         if node.parent is None:
                             return "No node found with value {0} in this binary tree.".format(target_value)
                         else:
-                            return Service.findValue(node.parent, target_value)
+                            return Service.find_value(node.parent, target_value)
                     else:
-                        return Service.findValue(node.right, target_value)
+                        return Service.find_value(node.right, target_value)
                 else:
-                    return Service.findValue(node.left, target_value)
+                    return Service.find_value(node.left, target_value)
